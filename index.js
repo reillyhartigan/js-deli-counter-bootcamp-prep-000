1d6571e9697b3bf52173a3978f14cf91deccc92a
sensible-state-8093 <reilly.hartigan@yale.edu>
2019-03-26 00:12:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-/* code here */
+/* code here for Take a Number Feature */
+
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  for (var i = 0; i < array.length; i++) {
+    array[i]
+  }
+  return "Welcome `${name}`. You are number `${}`"
+}
